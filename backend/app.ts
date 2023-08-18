ae650e07d2a17246344161227d77fb1524521da8
ihccenen <wirkz01@gmail.com>
2023-08-18 04:32:14 -0300

    added passport config and error, not found

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import session from 'express-session';
+import passport from 'passport';
 import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
+import bcrypt from 'bcryptjs';
+import { Strategy as LocalStrategy } from 'passport-local';
 import connectDB from './config/db';
-import userRouter from './routes/user'
+import userRouter from './routes/user';
+import User from './models/user';
+import { errorHandler, notFound } from './middleware/errorMiddleware';
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,8 +23,45 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: '*' }));
 app.use(helmet());
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(morgan('dev'));
 
+passport.use(
+  new LocalStrategy(async (username: string, password: string, done) => {
+    try {
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: 'Incorrect password' });
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser(function (user: any, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id)
+    .then((res) => done(null, res))
+    .catch(done);
+});
+
 app.use('/api/users', userRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
