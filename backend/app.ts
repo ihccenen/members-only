46eac6d60fc628b478fd4a58606eb0cb0a63e463
ihccenen <wirkz01@gmail.com>
2023-08-18 03:09:56 -0300

    added connect db and user route

--- backend/app.ts
+++ backend/app.ts
@@ -4,8 +4,12 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
+import connectDB from './config/db';
+import userRouter from './routes/user'
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
@@ -14,5 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: '*' }));
 app.use(helmet());
 app.use(morgan('dev'));
+
+app.use('/api/users', userRouter);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
