f0289e888c2555ec8ce015eca6dd7ac38d9ae47e
ihccenen <wirkz01@gmail.com>
2023-08-21 00:03:35 -0300

    deleted

--- backend/models/messege.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const { Schema } = mongoose;
-
-const MessageSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, required: true },
-  text: { type: String, required: true },
-});
