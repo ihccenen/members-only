b5c8198b0cd845be3d57198d3fb095738510ef96
ihccenen <wirkz01@gmail.com>
2023-08-22 13:18:23 -0300

    added error message

--- frontend/src/components/ErrorPage.tsx
+++ frontend/src/components/ErrorPage.tsx
@@ -1,9 +1,11 @@
-import { Link } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
 
 export default function ErrorPage() {
+  const error = useRouteError() as Error;
+
   return (
     <main>
-      <h1>Oh no, this route doesn't exist!</h1>
+      <h1>{ error.message }</h1>
       <Link to="/">
         You can go back to the home page by clicking here, though!
       </Link>
