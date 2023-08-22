92ad0b722f9dfdea9507804fc60a9c517ef7bb26
ihccenen <wirkz01@gmail.com>
2023-08-22 13:16:25 -0300

    semi-colon

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -31,7 +31,7 @@ const UserSchema = new Schema<IUser, UserModel, IUserMethods>(
 
 UserSchema.virtual('name').get(function () {
   return `${this.firstName} ${this.lastName}`;
-})
+});
 
 UserSchema.method('matchPassword', async function (enteredPassword: string) {
   return await bcrypt.compare(enteredPassword, this.password);
