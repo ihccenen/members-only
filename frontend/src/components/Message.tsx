87969375b984f490a026b9f46e1f557edccf09bd
ihccenen <wirkz01@gmail.com>
2023-08-24 14:32:08 -0300

    change navigate to /

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -24,7 +24,7 @@ export default function Message({
 
   const handleClick = () => {
     deleteMessage(_id)
-      .then(() => navigate('/messages'))
+      .then(() => navigate('/'))
       .catch((err) => setError(err.message));
   };
 
